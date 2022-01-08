4374bb3546dc379f7a1062c9b3437032a17efda3
jolitti <marcodeltrionfo@gmail.com>
2022-01-08 15:27:11 +0100

    Added comments for clarity

--- lib/Chess.h
+++ lib/Chess.h
@@ -4,18 +4,25 @@
 #include <string>
 using std::string;
 
+
+// Collection of all chess-related names
 namespace chess
-{
+{   
+    // Letters for the x coordinates on the chessboard
     const string LETTERS = "ABCDEFGH";
+    // Number of cells on a side of the board
     const int BOARD_SIZE = 8;
+    // Standard representation of a position outside the board (e.g. Z9)
     const string INVALID_POS = "INVALID";
 
+    // The possible colors a piece can have
     enum color
     {
         black,
         white
     };
 
+    // The specific type of piece
     enum piece
     {
         pawn,
