9363ab03ca9dd23870bfa8e49266761892ed1491
jolitti <marcodeltrionfo@gmail.com>
2022-01-11 14:48:29 +0100

    Accepted previous merge

--- lib/board/Board.h
+++ lib/board/Board.h
@@ -21,31 +21,11 @@ namespace chess
         // Default constructor (initial state of a chess board)
         Board();
 
-<<<<<<< HEAD
-        //print current state of the board
-        void print() const;
-
-        //add a new move into "moves" vector  
-        void save_move(Move move);
-
-        //returns the size of vector moves
-        unsigned int get_history_size() const;
-
-        //given index, returns associated move
-        Move get_move(unsigned int index) const;
-
-        //updates pieces vector with the given move: returns false if start is empty
-        bool set_move(Point start, Point end); 
-
-
-
-=======
         // Build board based on string representation
         Board(const string& seed);
 
         // Get corresponding piece in the 2d vector (or null if place is empty)
         Piece& getPieceAt(const Point& position) const;
->>>>>>> marco
     };
 }
 #endif
